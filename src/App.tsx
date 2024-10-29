b471d4b6797644f0534e7f525c3326be5e9f0fb1
Rudy Hentzen <rudy.hentzen@ft.com>
2024-10-29 18:36:54 +0000

    feat: add back in

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import { useDispatch } from "react-redux";
+import { setToken } from "./features/auth/authSlice";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import ProtectedRoutes from "./components/auth/ProtectedRoutes";
+import GlobalStyles from "./styles/GlobalStyles";
+import TasksPage from "./pages/TasksPage";
+
 function App() {
-  // const dispatch = useDispatch();
-  // const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
   // Rehydrate token from localStorage on app load
-  // useEffect(() => {
-  //   // const token = localStorage.getItem("token");
-  //   // if (token) {
-  //   //   dispatch(setToken(token));
-  //   // }
-  //   // setLoading(false);
-  // }, [dispatch]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(setToken(token));
+    }
+    setLoading(false);
+  }, [dispatch]);
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  return <div className="layout">hello</div>;
+  return (
+    <div className="layout">
+      <GlobalStyles />
+      <ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
+        <Router>
+          <Routes>
+            {/* Protected Routes for authenticated users */}
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/" element={<TasksPage />} />
+            </Route>
+
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+          </Routes>
+        </Router>
+      </ConfigProvider>
+    </div>
+  );
 }
 
 export default App;
